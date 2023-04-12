15c7f7b92b19f72a329dc85ad1ea6315cf6bc7ec
gigisoza <sozashviligigi@gmail.com>
2023-04-12 21:34:21 +0400

    Fetch data from api

--- src/components/TopArea.tsx
+++ src/components/TopArea.tsx
@@ -10,6 +10,34 @@ export const TopArea = ({ setUser }: TopAreaProps) => {
   const usernameRef = useRef<HTMLInputElement>(null);
   const [inputUser] = useState("octocat");
 
+  function handleSubmit() {
+    if (
+      usernameRef.current?.value.trim() === "" ||
+      usernameRef.current?.value === undefined
+    ) {
+      setEmpty(true);
+      setUser(null);
+      return;
+    }
+
+    setEmpty(false);
+    fetchUser(usernameRef.current.value);
+  }
+
+  async function fetchUser(username: string) {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
+
+    if (response.status != 200) {
+      setNotFound(true);
+      setUser(null);
+      return;
+    }
+
+    setNotFound(false);
+    console.log(data);
+  }
+
   return (
     <Container>
       <ThemeArea>
@@ -29,16 +57,37 @@ export const TopArea = ({ setUser }: TopAreaProps) => {
           {lightMode ? (
             <>
               DARK
-              <img src="/assets/icon-moon.svg" alt="dark mode" />
+              <img src="assets/icon-moon.svg" alt="dark mode" />
             </>
           ) : (
             <>
               LIGHT
-              <img src="/ssets/icon-sun.svg" alt="light mode" />
+              <img src="/assets/icon-sun.svg" alt="light mode" />
             </>
           )}
         </ChangeThemeBtn>
       </ThemeArea>
+
+      <InputArea
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <InputLabel>
+          <img src="../../public/assets/icon-search.svg" />
+        </InputLabel>
+
+        <Input
+          ref={usernameRef}
+          name="username"
+          id="username"
+          type="text"
+          placeholder="Search username ..."
+        />
+
+        <SubmitBtn type="submit">Search</SubmitBtn>
+      </InputArea>
     </Container>
   );
 };
@@ -68,5 +117,69 @@ const ChangeThemeBtn = styled.button`
 
   img {
     margin-left: 1.6rem;
+  }
+`;
+
+const InputArea = styled.form`
+  margin-top: 3.6rem;
+  border-radius: 1.5rem;
+  background: ${(props) => props.theme.colors.card};
+  box-shadow: 0px 16px 30px -10px rgba(70, 96, 187, 0.198567);
+  width: 100%;
+  height: 6rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.7rem 0.7rem 0.7rem 1.6rem;
+  transition: height 0.3s ease;
+  position: relative;
+  @media (min-width: 768px) {
+    height: 6.9rem;
+  }
+`;
+
+const InputLabel = styled.label`
+  height: 2.4rem;
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.4rem;
+  line-height: 192%;
+  color: ${(props) => props.theme.colors.textNorm};
+  background: none;
+  border: none;
+  margin: 0 0.8rem;
+  @media (min-width: 768px) {
+    font-size: 1.7rem;
+    margin: 0 2.4rem;
+  }
+  &:focus {
+    outline: 1px dashed #0079ff;
+  }
+`;
+
+const SubmitBtn = styled.button`
+  background: #0079ff;
+  border: none;
+  height: 100%;
+  border-radius: 1rem;
+  line-height: 2.1rem;
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #fff;
+  cursor: pointer;
+  width: 8.4rem;
+  transition: all 0.3s ease-out;
+  &:hover {
+    filter: brightness(1.05);
+    box-shadow: 0px 0px 15px -3px #0079ff;
+  }
+  @media (min-width: 768px) {
+    width: 10.6rem;
+    font-size: 1.7rem;
   }
 `;
